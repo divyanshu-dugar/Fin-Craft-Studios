ee93693b53ee1ae3d63e4bb22214ef701f041d97
Divyanshu Dugar <divyu2004@gmail.com>
2025-02-14 21:08:53 +0530

    removed console.log for retrieved expenses

--- client/src/ExpenseList.jsx
+++ client/src/ExpenseList.jsx
@@ -11,7 +11,6 @@ function ExpenseList(){
     useEffect(()=>{
         axios.get("http://localhost:8080/expense-list")
         .then(response => {
-            console.log(response.data);
             setExpenseList(response.data)
     })
         .catch(error => console.log(error))
