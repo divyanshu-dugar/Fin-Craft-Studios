cca4cdd922b63ddd33eeda32a8b8626b52394eda
Divyanshu Dugar <divyu2004@gmail.com>
2025-02-14 14:19:59 +0530

    navbar component and styling

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">💸 Ledgerify - Expense Tracker</div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/expense">Add Expense</Link>
+        </li>
+        <li>
+          <Link to="/expense-list">Expense List</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
