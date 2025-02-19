a3b4e978addcf9015c6ff99da25f9ac8d40b0566
Divyanshu Dugar <divyu2004@gmail.com>
2025-02-20 01:43:58 +0530

    added add category functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar"
 import Expense from "./Expense"
 import ExpenseList from "./ExpenseList"
 import EditExpense from './EditExpense';
+import AddCategory from './AddCategory';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/add-expense" element={<Expense />} />
           <Route path="/expense-list" element={<ExpenseList/>} />
           <Route path="/edit-expense/:id" element={<EditExpense/>} />
+          <Route path="/add-category" element={<AddCategory/>} />
           {/* 404 Page */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
