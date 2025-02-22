88529eeed0830903f2bcafcd9e5af1d3beff2b23
Divyanshu Dugar <divyu2004@gmail.com>
2025-02-22 19:59:51 +0530

    added income tracker and savings goal tracker with corresponding sub nav bar links

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -4,19 +4,32 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-logo"><Link className="navbar-logo" to="/">💸 Ledgerify - Expense Tracker</Link></div>
+      <div className="navbar-logo">
+        <Link className="navbar-logo" to="/">💸 Ledgerify</Link>
+      </div>
       <ul className="navbar-links">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/add-expense">Add Expense</Link>
+          <Link to="/income-list">Income Tracker</Link>
+          <ul>
+            <li><Link to="/add-income">Add Income</Link></li>
+            <li><Link to="/add-category">Add Category</Link></li>
+          </ul>
         </li>
         <li>
-          <Link to="/expense-list">Expense List</Link>
+          <Link to="/expense-list">Expense Tracker</Link>
+          <ul>
+            <li><Link to="/add-expense">Add Expense</Link></li>
+            <li><Link to="/add-category">Add Category</Link></li>
+          </ul>
         </li>
         <li>
-          <Link to="/add-category">Add Category</Link>
+          <Link to="/savings-goal-list">Savings Goal Tracker</Link>
+          <ul>
+            <li><Link to="/add-savings-goal">Add Savings Goal</Link></li>
+          </ul>
         </li>
       </ul>
     </nav>
