c0b5ac8ec765b37cda9168c050d894e63d3ca0e3
Divyanshu Dugar <divyu2004@gmail.com>
2025-02-22 23:11:33 +0530

    added all the new income related pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./Navbar"
-import Expense from "./Expense"
+import AddExpense from "./AddExpense"
 import ExpenseList from "./ExpenseList"
 import EditExpense from './EditExpense';
 import AddCategory from './AddCategory';
+import IncomeList from './IncomeList';
+import AddIncome from './AddIncome';
+import AddIncomeCategory from './AddIncomeCategory'
 
 function App() {
 
@@ -13,10 +16,13 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path='/' element={<ExpenseList/>}/>
-          <Route path="/add-expense" element={<Expense />} />
+          <Route path="/add-expense" element={<AddExpense />} />
+          <Route path="/add-category" element={<AddCategory/>} />
           <Route path="/expense-list" element={<ExpenseList/>} />
           <Route path="/edit-expense/:id" element={<EditExpense/>} />
-          <Route path="/add-category" element={<AddCategory/>} />
+          <Route path="/income-list" element={<IncomeList/>} />
+          <Route path="/add-income" element={<AddIncome />} />
+          <Route path="/add-income-category" element={<AddIncomeCategory />} />
           {/* 404 Page */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
