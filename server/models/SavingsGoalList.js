35265be33e5c57ee2ccb4edf0187767e35a6311d
Divyanshu Dugar <divyu2004@gmail.com>
2025-03-05 11:18:35 +0530

    implemented saving goal list model

---
+++ server/models/SavingsGoalList.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const savingsGoalList = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        amount: {type: Number, required: true},
+        deadline: {type: Date, required: true},
+        priority: {type: String, required: true}
+    }
+)
+
+module.exports = mongoose.model("SavingsGoalList", savingsGoalList);
