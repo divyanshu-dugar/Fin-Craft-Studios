d1d6d51969eca5e663f626af9853e676b022b7a9
Divyanshu Dugar <divyu2004@gmail.com>
2025-03-05 11:17:56 +0530

    implemented boilerplate code for savings goal list

---
+++ client/src/SavingsGoalList.jsx
@@ -0,0 +1,14 @@
+export default function SavingsGoalList(){
+    return(
+        <>
+            <h1>Savings Goal List</h1>
+            <div>
+                <h3>Title...</h3>
+                <p>Amount: $100</p>
+                <p>Deadline: 2023-02-15</p>
+                <p>Priority Level</p>
+                <p>Category</p>
+            </div>
+        </>
+    )
+}
