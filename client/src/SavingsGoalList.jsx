9609779ced5f196edf9c719d6667bd65884b7c5b
Divyanshu Dugar <divyu2004@gmail.com>
2025-03-08 18:14:06 +0530

    added boilterplate template for savings goal list

--- client/src/SavingsGoalList.jsx
+++ client/src/SavingsGoalList.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+
 export default function SavingsGoalList(){
+    let [savingsGoalList, setSavingsGoalList] = useState([{}]);
+
+    useEffect(() => {
+        axios.get("/saving-goals")
+        .then(response => setSavingsGoalList(response.data))
+        .catch(err => console.log(err));
+    },[])
+
     return(
         <>
             <h1>Savings Goal List</h1>
+            {savingsGoalList.map((goal) => {
             <div>
-                <h3>Title...</h3>
-                <p>Amount: $100</p>
+                <h3>Goal: {goal.name}</h3>
+                <p>Amount: {goal.amount}</p>
                 <p>Deadline: 2023-02-15</p>
                 <p>Priority Level</p>
                 <p>Category</p>
             </div>
+            })}
         </>
     )
 }
