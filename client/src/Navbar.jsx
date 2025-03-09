a2ad3c21168199fdc10c013073c3629428954bd8
Divyanshu Dugar <divyu2004@gmail.com>
2025-03-09 12:54:29 +0530

    modified navbar to include specific feature name

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -5,40 +5,63 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link className="navbar-logo" to="/">💸 Ledgerify</Link>
+        <Link className="navbar-logo" to="/">💸 Fin-Craft Studios</Link>
       </div>
       <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+
         <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/income-list">Income Tracker</Link>
-          <ul>
-            <li><Link to="/income-list">Income List</Link></li>
-            <li><Link to="/add-income">Add Income</Link></li>
-            <li><Link to="/add-income-category">Add Category</Link></li>
+          <span>Ledgerify</span>
+          <ul className="dropdown">
+            <li>
+              <span>Income Tracker</span>
+              <ul className="submenu">
+                <li><Link to="/income-list">Income List</Link></li>
+                <li><Link to="/add-income">Add Income</Link></li>
+                <li><Link to="/add-income-category">Add Category</Link></li>
+              </ul>
+            </li>
+            <li>
+              <span>Expenses Tracker</span>
+              <ul className="submenu">
+                <li><Link to="/expense-list">Expense List</Link></li>
+                <li><Link to="/add-expense">Add Expense</Link></li>
+                <li><Link to="/add-category">Add Category</Link></li>
+              </ul>
+            </li>
           </ul>
         </li>
+
         <li>
-          <Link to="/expense-list">Expense Tracker</Link>
-          <ul>
-            <li><Link to="/expense-list">Expense List</Link></li>
-            <li><Link to="/add-expense">Add Expense</Link></li>
-            <li><Link to="/add-category">Add Category</Link></li>
+          <span>Budgetify</span>
+          <ul className="dropdown">
+            <li><Link to="/">Coming Soon!🔒</Link></li>
+            <li><Link to="/">Coming Soon!🔒</Link></li>
           </ul>
         </li>
+
         <li>
-          <Link to="/savings-goal-list">Savings Goal Tracker</Link>
-          <ul>
+          <span>Investify</span>
+          <ul className="dropdown">
+            <li><Link to="/">Coming Soon!🔒</Link></li>
+            <li><Link to="/">Coming Soon!🔒</Link></li>
+          </ul>
+        </li>
+
+        <li>
+          <span>Savify</span>
+          <ul className="dropdown">
             <li><Link to="/savings-goal-list">Savings Goal List</Link></li>
             <li><Link to="/add-savings-goal">Add Savings Goal</Link></li>
           </ul>
         </li>
+
         <li>
-          <Link to="/tax-calculator">Tax Calculator</Link>
-        </li>
-        <li>
-          <Link to="/currency-converter">Currency Converter</Link>
+          <span>Other Tools</span>
+          <ul className="dropdown">
+            <li><Link to="/tax-calculator">Tax Calculator</Link></li>
+            <li><Link to="/currency-converter">Currency Converter</Link></li>
+          </ul>
         </li>
       </ul>
     </nav>
