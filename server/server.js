c759cefcdd80b94d4fb8ba186e4abaa455e4e14a
Divyanshu Dugar <divyu2004@gmail.com>
2025-09-29 14:14:51 -0400

    jwt setup done

--- server/server.js
+++ server/server.js
@@ -1,9 +1,45 @@
 const app = require('./app');
 const connectDB = require('./config/db');
+// Route imports
+const authRoutes = require('./routes/authRoutes'); 
 
 const PORT = process.env.PORT || 8080;
 
 connectDB();
+
+// JWT
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+
+// JSON Web Token Setup
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+
+// Configure its options
+let jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  console.log("payload received", jwt_payload);
+
+  if (jwt_payload) {
+    next(null, {
+      _id: jwt_payload._id,
+      userName: jwt_payload.userName,
+    });
+  } else {
+    next(null, false);
+  }
+});
+
+// middlewares
+passport.use(strategy);
+app.use(passport.initialize());
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
