f255cdeea35c4f982dee63362816760b56283aff
Divyanshu Dugar <divyu2004@gmail.com>
2025-11-28 20:47:36 -0500

    added authentication in all the routes

--- server/routes/income.js
+++ server/routes/income.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const { getIncomes, getIncomeById, addIncome, editIncome, deleteIncome } = require('../controllers/income');
-
 const router = express.Router();
+const {
+    getIncomes,
+    getIncomeById,
+    addIncome,
+    editIncome,
+    deleteIncome,
+    getIncomesByCategory,
+    getIncomesByDateRange,
+    getIncomeStats,
+    getIncomesByCategoryAndDateRange
+} = require('../controllers/income');
+const passport = require("passport")
 
-router.get('/', getIncomes);
-router.get('/:id', getIncomeById);
-router.post('/', addIncome);
-router.patch('/', editIncome);
-router.delete('/:id', deleteIncome);
+// All routes are protected
+router.get('/', passport.authenticate('jwt', { session: false }), getIncomes);
+router.get('/stats', passport.authenticate('jwt', { session: false }), getIncomeStats);
+router.get('/category/:category', passport.authenticate('jwt', { session: false }), getIncomesByCategory);
+router.get('/date-range', passport.authenticate('jwt', { session: false }), getIncomesByDateRange);
+router.get('/category/:category/date-range', passport.authenticate('jwt', { session: false }), getIncomesByCategoryAndDateRange); 
+router.get('/:id', passport.authenticate('jwt', { session: false }), getIncomeById);
+router.post('/', passport.authenticate('jwt', { session: false }), addIncome);
+router.put('/:id', passport.authenticate('jwt', { session: false }), editIncome);
+router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteIncome);
 
 module.exports = router;
